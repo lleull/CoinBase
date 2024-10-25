f4caf0f9da9563f876dfeef8320d2bc9294f48c4
lleull <leulmekonnen049@gmail.com>
2024-10-25 21:23:47 +0300

    everything back to where it was

--- src/App.js
+++ src/App.js
@@ -8,22 +8,10 @@ import axios from "axios";
 import Lastpage from "./Components/Lastpage/Lastpage";
 import { useState, useMemo } from "react";
 const App = () => {
-  const [dark, setDark] = useState(false)
-  const [number, setnumber] = useState(10)
-
-  const ThemeConfiguration = {
-    backgroundColor: dark ? "black" : "white",
-    color: dark ? "white" : "black",
-  }
-
-
-
-  const doubler = useMemo(() => { return sumNumbers(number) }, [number])
 
 
   return (
     <div style={ThemeConfiguration} className="app">
-      <button style={ThemeConfiguration} onClick={() => setDark(!dark)}>Change Theme</button>
 
       <div className="section">
         <Navbar />
@@ -32,8 +20,7 @@ const App = () => {
         <Joinus />
       </div>
       <Lastpage />
-      <input type="number" onChange={(e) => setnumber(e.target.value)} />
-      <h1 style={{ color: "red" }}>{doubler}</h1>
+
     </div>
   );
 };

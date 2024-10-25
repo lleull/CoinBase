4a73701b68cdab22c67151a9415ae050aa047d14
lleull <leulmekonnen049@gmail.com>
2024-10-25 21:22:10 +0300

    Homepage api changed

--- src/App.js
+++ src/App.js
@@ -6,21 +6,41 @@ import Choose from "./Components/ChooseUs/Choose";
 import Joinus from "./Components/Joinus/Joinus";
 import axios from "axios";
 import Lastpage from "./Components/Lastpage/Lastpage";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 const App = () => {
- 
+  const [dark, setDark] = useState(false)
+  const [number, setnumber] = useState(10)
+
+  const ThemeConfiguration = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  }
+
+
+
+  const doubler = useMemo(() => { return sumNumbers(number) }, [number])
+
 
   return (
-    <div className="app">
-      <div className="section">
+    <div style={ThemeConfiguration} className="app">
+      <button style={ThemeConfiguration} onClick={() => setDark(!dark)}>Change Theme</button>
+
+      {/* <div className="section">
         <Navbar />
         <Market />
         <Choose />
         <Joinus />
       </div>
-      <Lastpage />
+      <Lastpage /> */}
+      <input type="number" onChange={(e) => setnumber(e.target.value)} />
+      <h1 style={{ color: "red" }}>{doubler}</h1>
     </div>
   );
 };
 
 export default App;
+
+
+function sumNumbers(num) {
+  return num * 2
+}
